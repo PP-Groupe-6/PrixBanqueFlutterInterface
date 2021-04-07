b4186bd5ee8872d7df86bbe8308f7cc2b86e0f81
Corentin Laloz <corentin.laloz@hotmail.fr>
2021-04-07 09:58:37 -0400

    Authentification system start

---
+++ prix_banque_flutter_interface/lib/home_page.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+
+import 'authentification_service.dart';
+
+import 'package:provider/provider.dart';
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("HOME"),
+            ElevatedButton(
+              onPressed: () {
+                context.read<AuthenticationService>().signOut();
+              },
+              child: Text("Sign out"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
