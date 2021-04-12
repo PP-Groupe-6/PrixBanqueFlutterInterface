1b6e7a4e0024b0da59b191ab109d2e4e3597cd3a
Mople <49044525+Mople@users.noreply.github.com>
2021-04-12 22:55:45 +0200

    Display list of invoices from a json file

--- prix_banque_flutter_interface/lib/invoice_package/invoice_main_page.dart
+++ prix_banque_flutter_interface/lib/invoice_package/invoice_main_page.dart
@@ -1,27 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:prix_banque_flutter_interface/invoice_package/invoice_info_page.dart';
-
 import 'package:provider/provider.dart';
+import 'dart:convert';
+
+InvoiceList invoiceListFromJson(String str) => InvoiceList.fromJson(json.decode(str));
+
+class InvoiceList {
+  List<Invoice> invoices;
+
+  InvoiceList({ this.invoices,});
+
+  factory InvoiceList.fromJson(Map<String, dynamic> json) => InvoiceList(
+    invoices: List<Invoice>.from(json["invoices"].map((x) => Invoice.fromJson(x))),
+  );
+}
 
 class Invoice {
-  final String nameOfReceiver;
-  final DateTime date;
+  final int id;
   final int amount;
+  final String state;
+  final String expirationDate;
+  final String clientToPay;
+  final String email;
+  final String phoneNumber;
 
-  Invoice(this.nameOfReceiver,this.date,this.amount);
+  Invoice({this.id, this.amount, this.state, this.expirationDate, this.clientToPay, this.email, this.phoneNumber});
+
+  factory Invoice.fromJson(Map<String, dynamic> json){
+    return Invoice(
+      id : json['id'] as int,
+      amount: json['amount'] as int,
+      state: json['state'] as String,
+      expirationDate: json['expDate'] as String,
+      clientToPay: json['payto'] as String,
+      email: json['email'] as String,
+      phoneNumber: json['phone'] as String,
+    );
+  }
+}
+Future<String> loadAsset() async {
+  return await rootBundle.loadString('test_invoices.json');
 }
 
 class InvoicePage extends StatelessWidget {
   static const name = "/invoicePage";
 
-  List<Invoice> invoices = List.generate(
-      10,
-      (index) => Invoice(
-        "Person $index",
-        DateTime.utc(2021,4,index),
-        100,
-      )
-  );
+
+
+
+
 
 
   @override
@@ -30,24 +58,41 @@ class InvoicePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Your invoices"),
       ),
-      body: ListView.builder(
-          itemCount: invoices.length,
-          itemBuilder: (context,index){
-            return ListTile(
-              title : Text(invoices[index].nameOfReceiver),
-              onTap : (){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context)=>InvoiceInfo(
-                          invoice: invoices[index]
+      body: FutureBuilder(
+        future: rootBundle.loadString('test_invoices.json'),
+        builder: (BuildContext context, AsyncSnapshot snap){
+          if (snap.hasData){
+            var invoiceList = invoiceListFromJson(snap.data);
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.lime,
+                title: Text("Invoices to pay")
+              ),
+              body: Column(
+                children: invoiceList.invoices.map((invoice) => ListTile(
+                    title : Text(invoice.clientToPay),
+                    onTap : (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context)=>InvoiceInfo(
+                              invoice: invoice
+                          ),
                         ),
-                    ),
-                );
-              }
+                      );
+                    },
+                )
+                ).toList(),
+              ),
             );
+
           }
+          else{
+            return CircularProgressIndicator();
+          }
+        }
       )
+
     );
   }
 
