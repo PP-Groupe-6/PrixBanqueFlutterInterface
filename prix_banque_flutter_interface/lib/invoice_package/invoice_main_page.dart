0eedad10a93e66c6d2f989206ce73afc973e6df6
Mople <49044525+Mople@users.noreply.github.com>
2021-04-13 23:28:10 +0200

    Use display widget and invoice structure to clear file

--- prix_banque_flutter_interface/lib/invoice_package/invoice_main_page.dart
+++ prix_banque_flutter_interface/lib/invoice_package/invoice_main_page.dart
@@ -1,45 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:prix_banque_flutter_interface/invoice_package/invoice_info_page.dart';
-import 'package:provider/provider.dart';
-import 'dart:convert';
+import 'package:prix_banque_flutter_interface/invoice_package/display_list_invoices.dart';
 
-InvoiceList invoiceListFromJson(String str) => InvoiceList.fromJson(json.decode(str));
-
-class InvoiceList {
-  List<Invoice> invoices;
-
-  InvoiceList({ this.invoices,});
-
-  factory InvoiceList.fromJson(Map<String, dynamic> json) => InvoiceList(
-    invoices: List<Invoice>.from(json["invoices"].map((x) => Invoice.fromJson(x))),
-  );
-}
-
-class Invoice {
-  final int id;
-  final int amount;
-  final String state;
-  final String expirationDate;
-  final String clientToPay;
-  final String email;
-  final String phoneNumber;
-
-  Invoice({this.id, this.amount, this.state, this.expirationDate, this.clientToPay, this.email, this.phoneNumber});
-
-  factory Invoice.fromJson(Map<String, dynamic> json){
-    return Invoice(
-      id : json['id'] as int,
-      amount: json['amount'] as int,
-      state: json['state'] as String,
-      expirationDate: json['expDate'] as String,
-      clientToPay: json['payto'] as String,
-      email: json['email'] as String,
-      phoneNumber: json['phone'] as String,
-    );
-  }
-}
-
+import 'invoice.dart';
 
 class InvoicePage extends StatelessWidget {
   static const name = "/invoicePage";
@@ -52,47 +15,32 @@ class InvoicePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Container(
-          child: FutureBuilder(
-            future: rootBundle.loadString('test_invoices.json'),
-            builder: (BuildContext context, AsyncSnapshot snap){
-              if (snap.hasData){
-                var invoiceList = invoiceListFromJson(snap.data);
-                return Scaffold(
-                  appBar: AppBar(
-                    backgroundColor: Colors.redAccent,
-                    title: Text("Invoices to pay")
+        child: FutureBuilder(
+          future: rootBundle.loadString('test_invoices.json'),
+          builder: (BuildContext context, AsyncSnapshot snap){
+            if (snap.hasData){
+              var invoiceList = invoiceListFromJson(snap.data);
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  DisplayListInvoice(
+                    state: "Invoices to pay",
+                    invoices: invoiceList.invoicesToPay,
+                    color: Colors.redAccent,
                   ),
-                  body: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.redAccent)
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: invoiceList.invoices.map((invoice) => ListTile(
-                          title : Text(invoice.clientToPay),
-                          onTap : (){
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context)=>InvoiceInfo(
-                                    invoice: invoice
-                                ),
-                              ),
-                            );
-                          },
-                      )
-                      ).toList(),
-                    ),
-                  ),
-                );
+                  DisplayListInvoice(
+                    state: "Invoices paid",
+                    invoices: invoiceList.invoicesPaid,
+                    color: Colors.lightGreen,
+                  )
+                ],
+              );
 
-              }
-              else{
-                return CircularProgressIndicator();
-              }
             }
-          ),
+            else{
+              return CircularProgressIndicator();
+            }
+          }
         ),
       )
 
